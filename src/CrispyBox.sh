9ccbcf614fbd6def259303ae01246112f471e087
Chris Banwarth <github@aprettycoolprogram.com>
2023-12-05 17:53:46 -0500

    Update CrispyBox.sh

--- src/CrispyBox.sh
+++ src/CrispyBox.sh
@@ -11,23 +11,28 @@
 version=$("23.12")
 build=$("b231205")
 
-# Get the current CrispyBox scripts.
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_build.sh -P ~/.CrispyBox/Scripts/
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_clean.sh -P ~/.CrispyBox/Scripts/
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_deepclean.sh -P ~/.CrispyBox/Scripts/
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_optimize.sh -P ~/.CrispyBox/Scripts/
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_required.sh -P ~/.CrispyBox/Scripts/
-wget https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_update-upgrade.sh -P ~/.CrispyBox/Scripts/
-
-chmod +x ~/.CrispyBox/Scripts/*
-
-if [ $1 == "update"]; then
+if [ $1 == "update" ]; then
     echo "upgrading"
     ./CrispyBox/Scripts/CrispyBox_update-upgrade.sh $2
-elif [ $1 == "optimize"]; then
+elif [ $1 == "optimize" ]; then
     echo "optimizing"
     ./CrispyBox/Scripts/CrispyBox_optimize.sh $2
+elif [ $1 == "refresh" ]; then
+    RefreshCrispyBox
 else
-    echo "nothing"
+    echo "Nothing to do."
 fi
 
+RefreshCrispyBox(){
+    read -p "Press enter to refresh all CrispyBox scripts..."
+
+    # Get the current CrispyBox scripts.
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_build.sh -P ~/.CrispyBox/Scripts/
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_clean.sh -P ~/.CrispyBox/Scripts/
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_deepclean.sh -P ~/.CrispyBox/Scripts/
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_optimize.sh -P ~/.CrispyBox/Scripts/
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_required.sh -P ~/.CrispyBox/Scripts/
+    wget -o https://raw.githubusercontent.com/APrettyCoolProgram/CrispyBox/main/src/CrispyBox_update-upgrade.sh -P ~/.CrispyBox/Scripts/
+
+    chmod +x ~/.CrispyBox/Scripts/*
+}
